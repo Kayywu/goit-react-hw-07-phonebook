fc920c5bfad027549467bc4a83b349a80c386a08
Kayywu <katherine.wosik@gmail.com>
2024-03-03 10:31:10 +0100

    contacts

--- src/Redux/contactSlice.js
+++ src/Redux/contactSlice.js
@@ -6,25 +6,6 @@ export const fetchContacts = createAsyncThunk('contacts/fetchContacts', async ()
   return data;
 });
 
-/* export const addContact = createAsyncThunk('contacts/addContact', async (contact) => {
-  const response = await fetch('https://65b2a9f49bfb12f6eafe418a.mockapi.io/contacts/contacts', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(contact),
-  });
-  const data = await response.json();
-  return data;
-}); */
-
-/*export const deleteContact = createAsyncThunk('contacts/deleteContact', async (contactId) => {
-  await fetch(`https://65b2a9f49bfb12f6eafe418a.mockapi.io/contacts/contacts/${contactId}`, {
-    method: 'DELETE',
-  });
-  return contactId;
-});   */
-
 
 const contactSlice = createSlice({
   name: 'contacts',
--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import App from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
 import store from './Redux/root';
+import { fetchContacts } from './Redux/contactSlice';
 
+store.dispatch(fetchContacts());
 
 ReactDOM.render(
   <React.StrictMode>
